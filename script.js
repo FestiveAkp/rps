9632e6a48b7226a1308288fdcbc12ac0ab9f15c2
FestiveAkp <akashp807@gmail.com>
2018-02-10 23:51:00 -0600

    Introduce Skeleton boilerplate, refactor html/js, add timing element

--- script.js
+++ script.js
@@ -1,31 +1,61 @@
 "use strict"
 
-function playRPS(userIn) {
+async function playRPS(userIn) {
     const RPS = {
         1 : 'rock',
         2 : 'paper',
         3 : 'scissors'
     };
+    // Promise??? what even
+    var wait = ms => new Promise((r, j)=>setTimeout(r, ms));
+    var waitMore = ms => new Promise((r, j)=>setTimeout(r, ms));
+
     let outcomeField = document.getElementById('outcomeField');
     let userField = document.getElementById('userField');
     let computerField = document.getElementById('computerField');
     
+    resetFields();
+    disableButtons();
+
     let computerIn = getComputerIn();
 
     userField.innerHTML = `You played ${RPS[userIn.toString()]}`;
+    await wait(1000);
     computerField.innerHTML = `The computer played ${RPS[computerIn.toString()]}`;
+    await waitMore(1000);
 
-    let outcome = roundOutcome(userIn, computerIn);
+    let outcome = getRoundOutcome(userIn, computerIn);
     
     outcomeField.innerHTML = `You ${outcome}`;
     updateScoreTable(outcome);
+    enableButtons();
+}
+
+function resetFields () {
+    userField.innerHTML = '&nbsp';
+    computerField.innerHTML = '&nbsp';
+    outcomeField.innerHTML = '&nbsp';
+}
+
+function disableButtons() {
+    let rockBtn = document.getElementById('rock');
+    rockBtn.disabled = true;
+    rockBtn.className = 'button-disabled';
+
+    let paperBtn = document.getElementById('paper');
+    paperBtn.disabled = true;
+    paperBtn.className = 'button-disabled';
+
+    let scissorsBtn = document.getElementById('scissors');
+    scissorsBtn.disabled = true;
+    scissorsBtn.className = 'button-disabled';
 }
 
 function getComputerIn() {
     return Math.floor(Math.random() * (4-1) + 1);
 }
 
-function roundOutcome(userIn, computerIn) {
+function getRoundOutcome(userIn, computerIn) {
     if (userIn == computerIn) return 'tied';
     if (userIn == 1 && computerIn == 3) return 'win'; // play rock, against scissors
     if (userIn == 2 && computerIn == 1) return 'win'; // play paper, against rock
@@ -46,4 +76,18 @@ function updateScoreTable(outcome) {
         i++;
         computerScore.innerHTML = i;
     }
+}
+
+function enableButtons() {
+    let rockBtn = document.getElementById('rock');
+    rockBtn.disabled = false;
+    rockBtn.className = 'button-primary';
+
+    let paperBtn = document.getElementById('paper');
+    paperBtn.disabled = false;
+    paperBtn.className = 'button-primary';
+
+    let scissorsBtn = document.getElementById('scissors');
+    scissorsBtn.disabled = false;
+    scissorsBtn.className = 'button-primary';
 }
