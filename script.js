410b8e3d01d79d7dd6729d41e161632fcaad96e0
FestiveAkp <akashp807@gmail.com>
2018-02-17 13:47:36 -0600

    Add "graphics", tie counter, CSS

--- script.js
+++ script.js
@@ -7,37 +7,60 @@ async function playRPS(userIn) {
         2 : 'paper',
         3 : 'scissors'
     };
-    // Promise??? what even
+    // Declare waiting processes
     var wait = ms => new Promise((r, j)=>setTimeout(r, ms));
     var waitMore = ms => new Promise((r, j)=>setTimeout(r, ms));
+    var waitEvenMore = ms => new Promise((r, j)=>setTimeout(r, ms));
+    var waitMoreSo = ms => new Promise((r, j)=>setTimeout(r, ms));
+    var waitAgain = ms => new Promise((r, j)=>setTimeout(r, ms));
+    var waitOnceMore = ms => new Promise((r, j)=>setTimeout(r, ms));
 
-    // Declare and format output text fields
+    // Declare and format DOM objects
     let outcomeField = document.getElementById('outcomeField');
     let userField = document.getElementById('userField');
     let computerField = document.getElementById('computerField');
+    let userPic = document.getElementById('userPic');
+    let computerPic = document.getElementById('computerPic');
     
-    resetFields();
+    resetDOM();
     disableButtons();
 
     let computerIn = getComputerIn();
 
-    userField.innerHTML = `You played ${RPS[userIn.toString()]}`;
-    await wait(1000);
-    computerField.innerHTML = `The computer played ${RPS[computerIn.toString()]}`;
-    await waitMore(1000);
-
+    // Shake
+    movePicsDown();
+    await wait(100);
+    movePicsUp();
+    await waitMore(100);
+    movePicsDown();
+    await waitEvenMore(100);
+    movePicsUp();
+    await waitMoreSo(100);
+    
+    // Change images to result
+    userPic.src = `img/${ RPS[userIn.toString()] }-right.png`;
+    computerPic.src = `img/${ RPS[computerIn.toString()] }-left.png`;
+    movePicsDown();
+    await waitAgain(100);
+    movePicsUp();
+    await waitOnceMore(100);
+    
     let outcome = getRoundOutcome(userIn, computerIn);
     
-    // Update output text fields with outcome
+    // Update DOM with outcome
+    userField.innerHTML = `You played ${RPS[userIn.toString()]}`;
+    computerField.innerHTML = `The computer played ${RPS[computerIn.toString()]}`;
     outcomeField.innerHTML = `You ${outcome}`;
     updateScoreTable(outcome);
     enableButtons();
 }
 
-function resetFields() {
+function resetDOM() {
     userField.innerHTML = '&nbsp';
     computerField.innerHTML = '&nbsp';
     outcomeField.innerHTML = '&nbsp';
+    userPic.src = 'img/rock-right.png';
+    computerPic.src = 'img/rock-left.png';
 }
 
 function disableButtons() {
@@ -59,6 +82,16 @@ function getComputerIn() {
     return Math.floor(Math.random() * (4-1) + 1);
 }
 
+function movePicsDown() {
+    userPic.style.marginTop = '0.25em';
+    computerPic.style.marginTop = '0.25em';
+}
+
+function movePicsUp() {
+    userPic.style.marginTop = '0em';
+    computerPic.style.marginTop = '0em';
+}
+
 function getRoundOutcome(userIn, computerIn) {
     if (userIn == computerIn) return 'tied';
     if (userIn == 1 && computerIn == 3) return 'win'; // play rock, against scissors
@@ -74,11 +107,19 @@ function updateScoreTable(outcome) {
         i++;
         playerScore.innerHTML = i;
     }
+
     if (outcome == 'lose') {
         let computerScore = document.getElementById('computerScore');
         let i = computerScore.innerHTML;
         i++;
         computerScore.innerHTML = i;
+    }
+
+    if (outcome == 'tied') {
+        let tiedScore = document.getElementById('tiedScore');
+        let i = tiedScore.innerHTML;
+        i++;
+        tiedScore.innerHTML = i;
     }
 }
 
